c92c8db7a57aa71fbb4131860beaace25b05047c
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-04 21:23:40 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,12 +1,20 @@
 
 def mario
+<<<<<<< HEAD
   
+=======
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+>>>>>>> be3cdf0e881a01b6a246043794810cc6a891782b
   phrase = "It's-a me, Mario!"
   puts phrase
   
 end
 def toadstool
+<<<<<<< HEAD
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+=======
+    
+>>>>>>> be3cdf0e881a01b6a246043794810cc6a891782b
   puts status
 end
 def link
